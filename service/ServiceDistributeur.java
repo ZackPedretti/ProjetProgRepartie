12885871d2fc385d487d1ef33bbc78d33115db99
TibereLN <tibere55190@gmail.com>
2024-06-04 11:16:19 +0200

    client and service

---
+++ service/ServiceDistributeur.java
@@ -0,0 +1,13 @@
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+        
+
+public interface ServiceDistributeur extends Remote {
+
+    public void enregistrerClient(ServiceTableauBlanc c) throws RemoteException;
+    public void distribuerMessage(Dessin d) throws RemoteException;
+
+    
+}
